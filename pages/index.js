5f9d7d751dc790775fe492bfc3bc923ffceb296f
Sohomx <sohom377@gmail.com>
2021-11-21 09:30:11 +0530

    start the build

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Lets build Airbnb</h1>
+
+      {/* Header */}
+      {/* Banner */}
     </div>
   );
 }
