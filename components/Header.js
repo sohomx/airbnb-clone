a878899260dd2f837edf663cfaf06db83d513c1c
Sohomx <sohom377@gmail.com>
2021-11-21 10:47:05 +0530

    done with left side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,24 @@
+import Image from "next/image";
+
 function Header() {
   return (
-    <div>
-      <h1>I am the Header</h1>
-
+    <header className="sticky top-0 z-50 grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
-      <div></div>
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
 
       {/* Middle */}
       <div></div>
 
       {/* Right */}
       <div></div>
-    </div>
+    </header>
   );
 }
 
