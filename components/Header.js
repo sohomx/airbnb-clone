09bd2e64c0d8e91317e5f4a7f8fcbb3bb71e6367
Sohomx <sohom377@gmail.com>
2021-11-21 16:32:23 +0530

    completed the header component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
@@ -21,11 +27,19 @@ function Header() {
           type="text"
           placeholder="Search for your destination"
         />
-        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-while rounded-full p-2 cursor-pointer md:mx-2" />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
       {/* Right */}
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
